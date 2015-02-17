33b3e93bd1ad2091061c126adb01a65bc7664935
Dr-Lord <Lord.Fletcher@outlook.com>
2015-02-17 00:26:06 +0000

    First Commit
    Intended project description.

---
+++ Analyser.hs
@@ -0,0 +1,25 @@
+---- Poker Analyser
+--
+--      Author:
+--          Dr-Lord
+--
+--      Version:
+--          0.0
+--
+--      Description:
+--          Poker analysing shell.
+--          Inupts:
+--              Player's cards, table cards (later on: player's fiches and even
+--              later other players' as well).
+--              Possible user request for (his or other's) probability of a
+--              specific hand.
+--          Outputs:
+--              Probabilty of player's or others' specific hand.
+--              Specific request response.
+--              (Later: suggested bet)
+--
+
+
+
+main = do
+    print "Hello, world!"
---
+++ README.md
@@ -0,0 +1,16 @@
+Poker Analyser
+===
+
+NOT NOT BEGUN YET.
+
+Poker analysing shell.
+    Inupts:
+        Player's cards, table cards (later on: player's fiches and even
+        later other players' as well).
+        Possible user request for (his or other's) probability of a
+        specific hand.
+    Outputs:
+        Probabilty of player's or others' specific hand.
+        Specific request response.
+        (Later: suggested bet)
+
