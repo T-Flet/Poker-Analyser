e4d4416586bcafbf81d430cf98af6f759436a8fa
Dr-Lord <Lord.Fletcher@outlook.com>
2015-02-18 01:01:09 +0000

    Added some types
    Added Card, Suit and Value ADTs, drafted Hand type.

--- Analyser.hs
+++ Analyser.hs
@@ -18,7 +18,18 @@
 --              Specific request response.
 --              (Later: suggested bet)
 --
+--   Sections:
+--       1 - Imports and Type declarations
+--
 
+---- 1 - IMPORTS AND TYPE DECLARATIONS -----------------------------------------
+
+data Card = Card {value :: Value, suit :: Suit} deriving (Eq, Ord, Show, Read)
+data Suit = Spades | Clubs | Diamonds | Hearts deriving (Eq, Ord, Show, Read)
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+            | Jack | Queen | King | Ace deriving (Eq, Ord, Show, Read)
+
+-- type Hand = (Card v1 s1, Card v2 s2) deriving (Eq, Ord, Show, Read)
 
 
 main = do
