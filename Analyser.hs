37ca03a9cc5b9915c7d75309abbdfc69fb6303c1
Dr-Lord <Lord.Fletcher@outlook.com>
2015-03-05 01:12:10 +0000

    Version 0.7
    Shortened Probability to Prob.
    Came to a conclusion with a few considerations.
    Completed bestHand function.
    Created list of "empty" probabilities in descending HandType.
    Set up and probably completed changeProbs function.
    Set up all functions which changeProbs needs.
    It is my birthday!

--- Analyser.hs
+++ Analyser.hs
@@ -4,7 +4,7 @@
 --          Dr-Lord
 --
 --      Version:
---          0.6 - 03-04/03/2015
+--          0.7 - 04-05/03/2015
 --
 --      Description:
 --          Poker analysing shell.
@@ -28,24 +28,29 @@
 
 ---- 0 - TO DO and TO CONSIDER -------------------------------------------------
 
---MAKE Probability A FUNCTOR? MONAD? WHICH IS PASSED AROUND.
---  BUT FUNCTORS AND MONADS TAKE ONLY ONE VALUE...
---AND WHEN 100% RETURN A Hand, FALL THROUGH A CHAIN OF SOME SORT WITH IT
---(THIS IN THE bestHand FUNCTION)
+-- PERHAPS GROUP straightProb AND highCardProb TOGETHER
 
---ADD THE WHOLE CARD LIST TO THE Probability DATA TYPE TO CARRY AROUND?
+-- INTRODUCE quality FIELD IN Prob, REPRESENTING HOW GOOD A HandType IT IS
+-- AMONG ALL POSSIBLE SAME HandTypes
+    -- PERHAPS IT SHOULD BE IN HandType INSTEAD?
+
+-- FUNCTION whatProb WHICH IS GIVEN THE PRESENT CARDS AND STUFF LIKE
+-- Either Value Suit OR [Card] AND RETURNS THE PROBABILITY OF GETTING SUCH A
+-- SET FROM THE PRESENT ONES
+    -- PERHAPS THE need FIELD IN Prob SHOULD BE OF THE TYPE OF THAT STUFF
+    -- OR Prob SHOULD ALSO HAVE A cards FIELD LIKE Hand.
 
 --IN WHICH CASE (OR NOT) MAKE IT A FUNCTOR OR MONAD SO THAT CARDS ARE PASSED TO
 --IT ONE BY ONE AND EVERYTHING GETS UPDATED?
 
 --IF THE "FINAL" FUNCTIONS WHICH CHECK THE CARDS ARE THE SAME IN ANY OF THESE SITUATIONS,
 --START WITH THOSE
---      [Card] -> Probability
+--      [Card] -> Prob
 
 --foldr'S step VS MONAD BEHAVIOUR
 
 -- STRUCTURING CAN BE THE FOLLOWING:
---  ONE FUNCTION TAKES THE TABLE AND RETURNS Probability OF ALL HandtypeS;
+--  ONE FUNCTION TAKES THE TABLE AND RETURNS Prob OF ALL HandtypeS;
 --  THEN THE PLAYER'S HAND IS TAKEN IN AND MAPPED OVER THE PROBABILITIES;
 --  EITHER STOP AT THE FIRST 100% OR DO THEM ALL (OR BE LAZY AFTER THE FIRST ONE)
 
@@ -54,7 +59,7 @@
 -- CONSIDER USING APPLICATIVE FUNCTOR STYLE FOR THIS FUNCTION, WITH CUSTOM FUNCTOR, PERHAPS
 
 --MAKE isStraightFlush return RoyalFlush or StraightFlush; IT OPTIMISES TIME
---MAKE THESE FUNCTIONS BE :: [Card] -> Probability
+--MAKE THESE FUNCTIONS BE :: [Card] -> Prob
 -- MAKE ALL THESE FUNCTIONS ASSUME THE PREVIOUS ONE HAS RUN?
 -- MAKE THEM WORK BY COUNTING THE CARDS THAT ARE NOT "OUT"?
 -- AND PERHAPS ALL POSSIBLE OTHER PLAYERS' HANDS?
@@ -63,8 +68,8 @@
 ---- 00 - TESTING DATA ---------------------------------------------------------
 
 -- let a = [Card Spades King, Card Hearts Queen, Card Clubs Jack]
--- let b = [Probability HighCard 1 [], Probability FullHouse 0.3 [Left Ace], Probability Straight 0.8 [Right Diamonds]]
--- let Hand = probsToHand (sort a) (reverse $ sort b)
+-- let b = [Prob HighCard 1 [], Prob FullHouse 0.3 [Left Ace], Prob Straight 0.8 [Right Diamonds]]
+-- let h = probsToHand (sort a) (reverse $ sort b)
 
 
 
@@ -96,7 +101,7 @@ instance Enum Card where
 data Hand = Hand {hKind :: HandType, cards :: [Card]}
                 deriving (Eq, Ord, Show)
 
-data Probability = Probability {pKind :: HandType, chance :: Float, need :: [Either Value Suit]}
+data Prob = Prob {pKind :: HandType, chance :: Float, need :: [Either Value Suit]}
                 deriving (Eq, Ord, Show)
 
 
@@ -113,28 +118,53 @@ main = do
 ---- 3 - OTHER FUNCTIONS -------------------------------------------------------
 
 -- bestHand :: [Card] -> Hand
--- bestHand cs = probsToHand scs . foldr getCard [] scs
+-- bestHand cs = probsToHand scs . foldr addCard noProbs scs
 --     where scs = sort cs
--- Perhaps accumulator should already contain a Probability for each hand? Or not
+--           addCard = changeProbs scs
 
-    -- Assumes Probabilities are sorted by descending kind
+    -- Assumes Probabilities are sorted by descending HandType
     -- and that at least one has a 100% chance
-probsToHand :: [Card] -> [Probability] -> Hand
-probsToHand scs prs = Hand ht scs
-    where ht = pKind . head . dropWhile ((/= 1) . chance) $ prs
+probsToHand :: [Card] -> [Prob] -> Hand
+probsToHand scs prs = Hand hT scs
+    where hT = pKind . head . dropWhile ((/= 1) . chance) $ prs
+
+    -- List of "empty" probabilities in descending HandType
+noProbs :: [Prob]
+noProbs = map (\hT-> Prob hT 0 []) . reverse . enumFrom $ (minBound :: HandType)
+
+    -- Change the existing probabilities on existing list of cards by taking a
+    -- new card into consideration
+-- changeProbs :: [Card] -> Card -> [Prob] -> [Prob]
+-- changeProbs scs c [rF, sF, fK, fH, fl, st, tK, tP, oP, hC] =
+--                 [rF', sF', fK', fH', fl', st', tK', tP', oP', hC']
+--     where (rF', sF', fl')           = flushesProbs  scs c
+--           (fK', fH', tK', tP', oP') = nOfAKindProbs scs c
+--           st                        = straightProb  scs c
+--           hC'                       = highCardProb  scs c
 
 
---getCard :: Card -> Probability -> Probability
--- Perhaps this will actually just become >>= or <*> or fmap
-
-
---aimingFor :: [Probability] -> Probability
+--aimingFor :: [Prob] -> Prob
 -- Perhaps this should just be bestChance
 
+    -- Returns Probabilities of RoyalFlush, StraightFlush and Flush
+-- flushesProbs :: [Card] -> Card -> (Prob,Prob,Prob)
 
---isStraightFlush :: [Card] -> Probability
---isStraightFlush cs = foldr step (pure :: Probability) cs ???????
 
---pure OR return, DEPENGING ON FUNCTOR OR MONAD CHOICE
---
+
+    -- Returns Probabilities of FourOfAKind, FullHouse, ThreeOfAKind, TwoPair and OnePair
+-- nOfAKindProbs :: [Card] -> Card -> (Prob,Prob,Prob,Prob,Prob)
+
+
+
+    -- Returns the Probability of a Straight
+-- straightProb :: [Card] -> Card -> Prob
+-- straightProb scs c =
+
+
+
+    -- Returns the Probability of a HighCard
+-- highCardProb :: [Card] -> Card -> Prob
+-- highCardProb scs c = Prob HighCard 1 []
+
+
 
